8b6786db8d4c827712e2ed7e087ae0ce81975563
hassan mohamed kamel <60411536+hassan-kamel@users.noreply.github.com>
2021-07-04 00:43:51 +0200

    notfication done

--- src/app/donation/donation.component.ts
+++ src/app/donation/donation.component.ts
@@ -14,7 +14,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class DonationComponent implements OnInit {
   form: FormGroup;
-  notifactionSent: boolean = true;
+  notifactionSent: boolean = false;
   constructor(
     public _Router: Router,
     private fb: FormBuilder,
@@ -33,9 +33,17 @@ export class DonationComponent implements OnInit {
     });
   }
   Id: any;
+  Data:any;
+  city:any;
   ngOnInit(): void {
     let x = this._router.url;
     this.Id = x.slice(6, x.length - 9);
+    this._hospitalService.GetHospitalData(this.Id)
+    .subscribe(res=>{
+      this.Data = res;
+      this.city=this.Data.location;
+
+    })
   }
 
   onSubmit() {
@@ -43,24 +51,31 @@ export class DonationComponent implements OnInit {
       title: this.form.get('title')?.value,
       body: this.form.get('body')?.value,
       bloodType: this.form.get('bloodType')?.value,
-      city: this.form.get('city')?.value,
+      //city: this.form.get('city')?.value,
+      city:this.city,
       patientName: this.form.get('patientName')?.value,
       numberOfBags: this.form.get('numberOfBags')?.value,
     };
     console.log(body);
+
     this._hospitalService.donation(body, this.Id).subscribe(
       (res) => {
-        if (res) {
+
           this.notifactionSent = true;
           this.form.reset();
           setTimeout(() => {
             this.notifactionSent = false;
           }, 3000);
-        }
+
       },
       (err) => {
         alert('no user found to send notfiactions');
       }
     );
+
+
+
+
+
   }
 }
